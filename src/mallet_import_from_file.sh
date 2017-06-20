61f61a0817573d40a15a47fc947866d877f970f9
alee35 <ashley_lee@brown.edu>
2017-06-20 15:08:40 -0400

    added mallet tests

--- src/mallet_import_from_file.sh
+++ src/mallet_import_from_file.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH -n 16 
+#SBATCH -n 1 
 #SBATCH -t 24:00:00
-#SBATCH --mem=256G
+#SBATCH --mem=64G
 
 module load mallet/2.0.8rc3
 
-mallet import-file --input /users/alee35/scratch/land-wars-devel-data/cleanbills-20170517.tsv --output /users/alee35/scratch/land-wars-devel-data/cleanbills-20170523.mallet --keep-sequence --remove-stopwords --extra-stopwords /users/alee35/scratch/land-wars-devel-data/HansardProperNames.txt 
+mallet import-file --input /users/alee35/scratch/land-wars-devel-data/cleanbills-20170517.tsv --output /users/alee35/scratch/land-wars-devel-data/cleanbills-20170607.mallet --keep-sequence --remove-stopwords --extra-stopwords /users/alee35/scratch/land-wars-devel-data/hansardpropernames.txt --print-output 
