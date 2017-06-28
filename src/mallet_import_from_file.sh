cae7cd75b5fe08285338c01c43db23586ab7ecbe
alee35 <ashley_lee@brown.edu>
2017-06-28 14:40:21 -0400

    concatenated two stopwords lists

--- src/mallet_import_from_file.sh
+++ src/mallet_import_from_file.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+#SBATCH -C intel
 #SBATCH -n 1 
 #SBATCH -t 24:00:00
 #SBATCH --mem=64G
 
 module load mallet/2.0.8rc3
 
-mallet import-file --input /users/alee35/scratch/land-wars-devel-data/cleanbills-20170517.tsv --output /users/alee35/scratch/land-wars-devel-data/cleanbills-20170607.mallet --keep-sequence --remove-stopwords --extra-stopwords /users/alee35/scratch/land-wars-devel-data/hansardpropernames.txt --print-output 
+mallet import-file --input /users/alee35/scratch/land-wars-devel-data/cleanbills-20170626.tsv --output /users/alee35/scratch/land-wars-devel-data/cleanbills-20170626.mallet --keep-sequence --remove-stopwords --extra-stopwords /users/alee35/code/inquiry-for-philologic-analysis/data/stopwords-20170628.txt 
--- src/mallet_train_lda.sh
+++ src/mallet_train_lda.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+#SBATCh -C intel
 #SBATCH -n 16 
 #SBATCH -t 48:00:00
 #SBATCH --mem=256G
 
 module load mallet/2.0.8rc3
 
-mallet train-topics --input /users/alee35/scratch/land-wars-devel-data/cleanbills-20170607.mallet --num-topics 500 --optimize-interval 10 --output-state /gpfs/data/datasci/alee35/land-wars-devel-data/topic_state_500.gz --output-topic-keys /gpfs/data/datasci/alee35/land-wars-devel-data/keys_500.txt --output-doc-topics /gpfs/data/datasci/alee35/land-wars-devel-data/composition_500.txt
+mallet train-topics --input /users/alee35/scratch/land-wars-devel-data/cleanbills-20170626.mallet --num-topics 500 --optimize-interval 10 --output-state /users/alee35/scratch/land-wars-devel-data/topic_state_500.gz --output-topic-keys /users/alee35/scratch/land-wars-devel-data/keys_500.txt --output-doc-topics /users/alee35/scratch/land-wars-devel-data/composition_500.txt
