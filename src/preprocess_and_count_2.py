391507eb5e3e19d54434e2ffdc9d54ee4925dc4d
ashley_lee <ashley_lee@brown.edu>
2017-08-09 14:16:21 -0400

    added debate ID

--- src/preprocess_and_count_2.py
+++ src/preprocess_and_count_2.py
@@ -58,7 +58,6 @@ def prepare_text(text):
         # # compute decade
         # # text['DECADE'] = (text['YEAR'].map(lambda x: int(x) - (int(x) % 10)))
         # remove non-alpha numeric characters from bill titles
-        # text.loc[index, 'BILL'] = text['BILL'].map(lambda x: re.sub(r'[^A-Za-z0-9 ]', '', str(x)))
         text.loc[index, 'BILL'] = str(row.BILL).translate(None, string.digits + string.punctuation)
         # convert integer speech acts to string and decode unicode strings
         if type(row['SPEECH_ACT']) != str and type(row['SPEECH_ACT']) != unicode:
@@ -71,16 +70,16 @@ def prepare_text(text):
     text['YEAR'].fillna(method='ffill', inplace=True)
     sys.stdout.write('forward fill dates!')
     sys.stdout.write('\n')
+    # concatenate BILL and DATE to get new DEBATE ID
+    text['BILL'] = text['BILL'] + ' ' + text['DATE']
+    sys.stdout.write('bill and date ID created!')
+    sys.stdout.write('\n')
     # drop some columns
     text.drop(['ID', 'DATE', 'MEMBER', 'CONSTITUENCY'], axis=1, inplace=True)
     # write to csv
     text.to_csv(path + 'membercontributions-20170807.tsv', sep='\t', index=False)
     sys.stdout.write('processed corpus written to TSV!')
     sys.stdout.write('\n')
-
-    # read from csv after writing once
-    # with open(path + 'membercontributions-20170807.tsv', 'r') as f:
-        # text = pd.read_csv(f, sep='\t')
 
     # groupby year, decade, bill, and concatenate speech act with a space
     text = text.groupby(['BILL', 'YEAR'])['SPEECH_ACT'].agg(lambda x: ' '.join(x)).reset_index()
@@ -133,14 +132,14 @@ def build_dict_replace_words(row, mdict):
     # replace words with stems or dummy
     veca = vec.toarray()
     # write metadata to file for mallet
-    with open(path + "mc-20170727-stemmed.txt", "a") as f:
+    with open(path + "mc-20170809-stemmed.txt", "a") as f:
         f.write(str(row[0]) + '\t' + str(row[1]) + '\t')
     # write speech act with stems or dummy
     for i in range(len(words)):
-        with open(path + "mc-20170727-stemmed.txt", "a") as f:
+        with open(path + "mc-20170809-stemmed.txt", "a") as f:
             f.write((str(mdict.get(words[i])) + ' ') * int(veca[:, i]))
     # insert new line character after each speech act
-    with open(path + "mc-20170727-stemmed.txt", "a") as f:
+    with open(path + "mc-20170809-stemmed.txt", "a") as f:
         f.write('\n')
         sys.stdout.write('speech act {} written to file'.format(index))
         sys.stdout.write('\n')
@@ -167,43 +166,22 @@ path_seed = '/gpfs/data/datasci/paper-m/data/seed/'
 # path = '/users/alee35/Google Drive/repos/inquiry-for-philologic-analysis/data/'
 # path_seed = '/users/alee35/Google Drive/repos/inquiry-for-philologic-analysis/data/'
 
-# # Load the raw data to a dataframe
-# with open(path + 'membercontributions-20161026.tsv', 'r') as f:
-#     text = pd.read_csv(f, sep='\t')
+# Load the raw data to a dataframe
+with open(path + 'membercontributions-20161026.tsv', 'r') as f:
+    text = pd.read_csv(f, sep='\t')
 # # with open(path + 'membercontributions_test.tsv', 'r') as f:
 #     # text = pd.read_csv(f, sep='\t')
-# sys.stdout.write('corpus read in successfully!')
-# sys.stdout.write('\n')
-
-# Prepare the Tecxt
-# text = prepare_text(text)
-
-# read from csv after writing once
-with open(path + 'membercontributions-20170807.tsv', 'r') as f:
-    text = pd.read_csv(f, sep='\t')
 sys.stdout.write('corpus read in successfully!')
 sys.stdout.write('\n')
-sys.stdout.write(text.columns.values)
-sys.stdout.write('\n')
 
-# groupby year, decade, bill, and concatenate speech act with a space
-text = text.groupby(['BILL', 'YEAR'])['SPEECH_ACT'].agg(lambda x: ' '.join(map(str, x))).reset_index()
-# append seeds to text
-with open(path_seed + 'four_corpus.txt', 'r') as f:
-    seed = pd.read_csv(f, sep='\t', header=None, names=['SPEECH_ACT'])
-# decode unicode string with unicode codec
-for index, row in seed.iterrows():
-    seed[index, "SPEECH_ACT"] = row["SPEECH_ACT"].decode('utf-8')
-# make metadataframe for seeds
-seed['BILL'] = ['Seed1-Napier', 'Seed2-Devon',
-                'Seed3-Richmond', 'Seed4-Bessborough']
-seed['YEAR'] = [1884, 1845, 1882, 1881]
-seed = seed[['BILL', 'YEAR', 'SPEECH_ACT']]
-# append to end of text df
-text = pd.concat([text, seed]).reset_index(drop=True)
-sys.stdout.write('added seed to corpus successfully!')
-sys.stdout.write('\n')
+# Prepare the Text
+text = prepare_text(text)
 
+# read from csv after doing prepare_text once
+# with open(path + 'membercontributions-20170807.tsv', 'r') as f:
+#     text = pd.read_csv(f, sep='\t')
+# sys.stdout.write('corpus read in successfully!')
+# sys.stdout.write('\n')
 
 # Initialize a dictionary of all unique words, stemmer and lemmatizer
 master_dict = {}
