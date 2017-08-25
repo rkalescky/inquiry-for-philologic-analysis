7fdc57c02cb4751b707a27e6547f0c0bafaf2338
ashley_lee <ashley_lee@brown.edu>
2017-08-25 17:39:19 -0400

    fix stopword bug, write out dict of all words and stems

--- src/preprocess_and_count_2.py
+++ src/preprocess_and_count_2.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import sys
 import string
+import csv
+import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk import pos_tag
 from nltk.corpus import wordnet as wn
@@ -97,12 +99,12 @@ def prepare_text(text):
     seed = seed[['BILL', 'YEAR', 'SPEECH_ACT']]
     # append to end of text df
     text = pd.concat([text, seed]).reset_index(drop=True)
-    
+
     # remove tabs from text columns
     # remove quotes too?
 
     # write to csv
-    text.to_csv(path + 'membercontributions-20170814.tsv', sep='\t', index=False)
+    text.to_csv(path + 'membercontributions-20170824.tsv', sep='\t', index=False)
     sys.stdout.write('corpus and seed processed and written successfully!')
     sys.stdout.write('\n')
 
@@ -111,7 +113,7 @@ def prepare_text(text):
 
 # function to build up dictionary of all unique words and replace words in corpus with stems
 # @profile
-def build_dict_replace_words(row, mdict):
+def build_dict_replace_words(row, mdict, custom_stopwords):
 
     # get unique words in speech act
     vectorizer = CountVectorizer()
@@ -123,16 +125,22 @@ def build_dict_replace_words(row, mdict):
     # check dictionary for words and add if not present
     # check if word is already in dict
     not_cached = [word for word in words if word not in mdict]
-    # check if word is dummy or not
+    # check if word is stopword, dummy, or not dummy
     dictionary = enchant.Dict("en_GB")
-    dummy = [word for word in not_cached if word.isalpha() is False or dictionary.check(word) is False]
+    stopword = [word for word in not_cached if word in custom_stopwords]
+    stopword_dict = dict(zip(stopword, ['stopwordstop'] * len(stopword)))
+    dummy = [word for word in not_cached if word not in custom_stopwords and
+             word.isalpha() is False or dictionary.check(word) is False]
     dummy_dict = dict(zip(dummy, ['williewaiola'] * len(dummy)))
     # stem or lemmatize
-    not_dummy = [word for word in words if word.isalpha() and dictionary.check(word)]
+    not_dummy = [word for word in words if word not in custom_stopwords and
+                 word.isalpha() and
+                 dictionary.check(word)]
     stems = [stemmer.stem(word) for word in not_dummy]
     # TOFIX: lemmas = lemmatize_pos(not_dummy)
     not_dummy_dict = dict(zip(not_dummy, stems))
     # update dictionary
+    mdict.update(stopword_dict)
     mdict.update(dummy_dict)
     mdict.update(not_dummy_dict)
     sys.stdout.write('number of keys in master dict = {}'.format(len(mdict)))
@@ -141,54 +149,75 @@ def build_dict_replace_words(row, mdict):
     # replace words with stems or dummy
     veca = vec.toarray()
     # write metadata to file for mallet
-    with open(path + "mc-20170816-stemmed.txt", "a") as f:
+    with open(path + "mc-20170824-stemmed.txt", "a") as f:
         f.write(str(row[0]) + '\t' + str(row[1]) + '\t')
     # write speech act with stems or dummy
     for i in range(len(words)):
-        with open(path + "mc-20170816-stemmed.txt", "a") as f:
+        with open(path + "mc-20170824-stemmed.txt", "a") as f:
             f.write((str(mdict.get(words[i])) + ' ') * int(veca[:, i]))
     # insert new line character after each speech act
-    with open(path + "mc-20170816-stemmed.txt", "a") as f:
+    with open(path + "mc-20170824-stemmed.txt", "a") as f:
         f.write('\n')
         sys.stdout.write('speech act {} written to file'.format(index))
         sys.stdout.write('\n')
+
+
+# Function to read dataframe and read empty df if no columns
+def read_data(file):
+    try:
+        df = pd.read_csv(file, sep='\t', skiprows=row.SEQ_IND, usecols=[2],
+                         quoting=csv.QUOTE_NONE)
+    except pd.io.common.EmptyDataError:
+        df = pd.DataFrame()
+    return df
 
 
 # function to count correctly spelled and incorrectly spelled words
 # @profile
 def count_words(row, mdict):
     # read sa from file and create sa vector
-    with open(path + 'mc-20170816-stemmed.txt', 'r') as f:
-        sa = pd.read_csv(f, sep='\t', skiprows=row.SEQ_IND, usecols=[2])
+    # with open(path + 'mc-20170824-stemmed.txt', 'r') as f:
+    #     sa = pd.read_csv(f, sep='\t', skiprows=row.SEQ_IND, usecols=[2],
+    #                      quoting=csv.QUOTE_NONE)
+    sa = read_data(path + 'mc-20170824-stemmed.txt')
     vectorizer2 = CountVectorizer(vocabulary=mdict)
     vec2 = vectorizer2.fit_transform(sa)
-    dummy_ind = vectorizer2.vocabulary_.get('williewaiola')
-    sys.stdout.write('speech act {} added to debate {} matrix'.format(row.SEQ_IND, name))
-    sys.stdout.write('\n')
+    if sa.shape[0] > 0:
+        dummy_ind = vectorizer2.vocabulary_.get('williewaiola')
+        stopword_ind = vectorizer2.vocabulary_.get('stopwordstop')
+        vec2 = vec2.toarray()
+        sys.stdout.write('speech act {} added to debate {} matrix'.format(row.SEQ_IND, name))
+        sys.stdout.write('\n')
+    else:
+        dummy_ind = 0
+        stopword_ind = 0
+        vec2 = np.zeros((1, len(mdict)))
+        sys.stdout.write('speech act {} EMPTY and zeroes added to debate {} matrix'.format(row.SEQ_IND, name))
+        sys.stdout.write('\n')
 
-    return(vec2.toarray(), dummy_ind)
+    return(vec2, dummy_ind, stopword_ind)
 
 
 # Set the paths
-path = '/gpfs/data/datasci/paper-m/data/speeches_dates/'
-path_seed = '/gpfs/data/datasci/paper-m/data/seed/'
-#path = '/users/alee35/Google Drive/repos/inquiry-for-philologic-analysis/data/'
-#path_seed = '/users/alee35/Google Drive/repos/inquiry-for-philologic-analysis/data/'
+# path = '/gpfs/data/datasci/paper-m/data/speeches_dates/'
+# path_seed = '/gpfs/data/datasci/paper-m/data/seed/'
+path = '/users/alee35/Google Drive/repos/inquiry-for-philologic-analysis/data/'
+path_seed = '/users/alee35/Google Drive/repos/inquiry-for-philologic-analysis/data/'
 
 # Load the raw data to a dataframe
 # with open(path + 'membercontributions-20161026.tsv', 'r') as f:
     # text = pd.read_csv(f, sep='\t')
-#with open(path + 'membercontributions_test.tsv', 'r') as f:
-#    text = pd.read_csv(f, sep='\t')
+with open(path + 'membercontributions_test.tsv', 'r') as f:
+   text = pd.read_csv(f, sep='\t')
 # sys.stdout.write('corpus read in successfully!')
 # sys.stdout.write('\n')
 
 # Prepare the Text
-# text = prepare_text(text)
+text = prepare_text(text)
 
 # Read from csv after doing prepare_text once
-with open(path + 'membercontributions-20170814.tsv', 'r') as f:
-   text = pd.read_csv(f, sep='\t')
+# with open(path + 'membercontributions-20170824.tsv', 'r') as f:
+   # text = pd.read_csv(f, sep='\t')
 sys.stdout.write('corpus read in successfully!')
 sys.stdout.write('\n')
 
@@ -207,10 +236,24 @@ master_dict = {}
 stemmer = SnowballStemmer('english')
 lemmatizer = WordNetLemmatizer()
 
+# Read in custom stopword lists
+with open(path + 'stoplists/en.txt') as f:
+    en_stop = f.read().splitlines()
+with open(path + 'stoplists/stopwords-20170628.txt') as f:
+    custom_stop = f.read().splitlines()
+custom_stopwords = en_stop + custom_stop
+
 # Write stemmed corpus to file
 for index, row in deb.iterrows():
     print(row)
-    build_dict_replace_words(row, master_dict)
+    build_dict_replace_words(row, master_dict, custom_stopwords)
+
+# Pickle Master Dictionary to check topic modeling later
+with open(path + 'master_dict.pickle', 'wb') as handle:
+    pickle.dump(master_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# Load and deserialize pickled dict
+# with open(path + 'master_dict.pickle', 'rb') as handle:
+#     master_dict = pickle.load(handle)
 
 # Vocabulary for counting words is unique set of values in master dict
 vocabulary = set(master_dict.values())
@@ -218,28 +261,39 @@ sys.stdout.write('vocabulary built successfully!')
 sys.stdout.write('\n')
 
 # Count words and build debate doc-term matrix
-group = text.groupby(["BILL", "YEAR"])
+group = text.groupby(["BILL"], sort=False)
 doc_term_matrix = np.zeros((len(group), len(vocabulary)), dtype=int)
+
+group_ind = 0
 for name, df in group:
-    group_ind = int(group.indices.get(name))
-    seq_ind = df.index.tolist()
+    # need debate index, speech act index, and sa within debate index
+    # group_ind = group.indices.get(name)     # debate index
+    print("group_ind: " + str(group_ind))
+    seq_ind = df.index.tolist()             # sa index
     df = df.assign(SEQ_IND=seq_ind)
-    df.reset_index(inplace=True)
+    df.reset_index(inplace=True)            # sa w/i debate index
+    # initialize debate matrix
     num_docs = df.shape[0]
     debate_matrix = np.zeros((num_docs, len(vocabulary)), dtype=int)
+    # fill debate matrix, speech act by speech act
     for index, row in df.iterrows():
-        sa_vec, dummy_ind = count_words(row, vocabulary)
+        sa_vec, dummy_ind, stopword_ind = count_words(row, vocabulary)
         debate_matrix[index, ] = sa_vec
     # sum debate matrix rows to get debate vector
     debate_vec = debate_matrix.sum(axis=0)
     # build document term matrix, debate by debate
     doc_term_matrix[group_ind, ] = debate_vec
+    # add one to debate index
+    group_ind += 1
 sys.stdout.write('doc-term matrix built successfully!')
 sys.stdout.write('\n')
 
 # Print number of correctly/incorrectly spelled words
+nr_stopwords = doc_term_matrix[:, stopword_ind].sum()
 nr_incorrectly_sp = doc_term_matrix[:, dummy_ind].sum()
 nr_correctly_sp = doc_term_matrix.sum() - nr_incorrectly_sp
+sys.stdout.write('Number stopwords: ' + str(nr_stopwords))
+sys.stdout.write('\n')
 sys.stdout.write('Number incorrectly spelled: ' + str(nr_incorrectly_sp))
 sys.stdout.write('\n')
 sys.stdout.write('Number correctly spelled: ' + str(nr_correctly_sp))
