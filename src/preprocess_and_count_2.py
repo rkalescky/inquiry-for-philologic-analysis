22616812150af9ad7e4a8a212cbd598b3b203d61
alee35 <ashley.syg.lee@gmail.com>
2017-11-20 13:45:50 -0500

    test

--- src/preprocess_and_count_2.py
+++ src/preprocess_and_count_2.py
@@ -208,6 +208,7 @@ path = '/gpfs/data/datasci/paper-m/data/speeches_dates/'
 path_seed = '/gpfs/data/datasci/paper-m/data/seed/'
 # path = '/users/alee35/Google Drive/repos/inquiry-for-philologic-analysis/data/'
 # path_seed = '/users/alee35/Google Drive/repos/inquiry-for-philologic-analysis/data/'
+sys.stdout = open('../logs/log.txt', 'w')
 
 # Load the raw data to a dataframe
 #with open(path + 'membercontributions-20161026.tsv', 'r') as f:
