a987e77ca7df322beb5438aec10e1f2bdcb9b748
alee35 <ashley.syg.lee@gmail.com>
2018-10-04 14:53:27 -0400

    fixed fpath

--- src-gen/mallet_import_from_file.sh
+++ src-gen/mallet_import_from_file.sh
@@ -5,6 +5,6 @@
 #SBATCH -t 24:00:00
 #SBATCH --mem=64G
 
-fpath = '/gpfs/data/datasci/paper-m/data'
+fpath='/gpfs/data/datasci/paper-m/data'
 
-mallet import-file --input $(fpath)/mc-20170814-stemmed.txt --output $(fpath)/cleanbills-20170820.mallet --keep-sequence --remove-stopwords --extra-stopwords ../data/stoplists/stopwords-20170628.txt
+mallet import-file --input $fpath/mc-20170814-stemmed.txt --output $fpath/cleanbills-20170820.mallet --keep-sequence --remove-stopwords --extra-stopwords ../data/stoplists/stopwords-20170628.txt
